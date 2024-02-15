b8f8bc035fdbd45e05306564be80fc70d39b25ef
Akhilesh Malakar <137974715+Akhileshmalakar@users.noreply.github.com>
2024-02-15 13:46:49 +0000

    Convesion of Datatypes

---
+++ 01_basic/03_ConversionOperation.js
@@ -0,0 +1,47 @@
+// let score  = 50 
+// let score = "50";
+// let score = "50ab";
+// let score = null
+// let score = undefined;
+// let score = true;
+// let score = "Pappu";
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+//   Conversion data types 
+
+// string ko number me convert krne ke liye
+// let valueNumber = Number(score);
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+
+// "50" => 50
+// "50ab" => NaN
+// null => 0
+// undefine => undefine
+// true => 1(ulta);
+// "Pappu" => NaN
+
+
+// 1 ko convert boolen vlaue me 
+// let Islogin = 1;
+// let Islogin = "";
+// let Islogin = "Acha ji";
+
+// let boolenIslogin = Boolean(Islogin);
+// console.log(boolenIslogin);
+
+//  Output 
+// 1 => True; &  0 => false;
+// "" => fasle 
+// "Acha ji " => true;      
+
+
+let someNumber = 33;
+
+let StringNumber = String(someNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
