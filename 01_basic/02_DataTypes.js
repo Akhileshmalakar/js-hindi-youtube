56eef2fcf7103c9944f12f01523f01f88aaf06ae
Akhilesh Malakar <137974715+Akhileshmalakar@users.noreply.github.com>
2024-02-15 12:34:58 +0000

    container config done

---
+++ 01_basic/02_DataTypes.js
@@ -0,0 +1,27 @@
+"use strict"   /// treat all js code newer version
+
+// alert ( 3 + 3 ); we are using nodejs, Not browser
+
+console.log(3+3)   // code readability high
+
+console.log("Akku");
+
+// data types
+
+let name = "Akhilesh";    // stirng data types
+let age = 18;             // integer 
+let Islogin = true/false  // bool 
+
+
+
+// number  = 2 to power 53 
+// bigint;   age number bahut jyd bada he to bigint use karte he(traiding me ,stock market, badi company me , etc)
+// string => "" 
+// boolen => ture ya false
+// null  => standalone value he 
+// undifine =>  when value is not digifine 
+// symbol =>   Uniques find karne ke liye use 
+
+// Object
+
+console.log(typeof,"1234");
