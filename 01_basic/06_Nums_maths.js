7d38cdafb03abee90e2734d56769790a2122d422
Akhilesh Malakar <137974715+Akhileshmalakar@users.noreply.github.com>
2024-02-23 18:02:49 +0000

    math and numb

---
+++ 01_basic/06_Nums_maths.js
@@ -0,0 +1,45 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toExponential());
+// console.log(balance.toString());
+// console.log(balance.toFixed(3));
+
+// console.log(balance.toString().length);
+
+// const otherNumber = 123.8322
+// console.log(otherNumber.toPrecision(3));
+
+// const Hundreds = 100000;
+// // console.log(Hundreds.toLocaleString());  // use in USA doller me 
+// console.log(Hundreds.toLocaleString('en-IN'));
+
+
+
+
+// ++++++++++++++++++++ Maths ++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4));   // esme .5 pr depend kerega  
+// console.log(Math.ceil(4.4));    // esme top wali value print hogi
+// console.log(Math.floor(4.4));   // esme hamesa 4 hi value lega 
+// console.log(Math.min(4,5,5,6,2));
+// console.log(Math.max(4,9,7,1,5));
+
+
+
+// console.log(Math.random());     // ye hamesa 0 or 1 ke bich value deti he
+// console.log((Math.random()*10) + 1);    
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
