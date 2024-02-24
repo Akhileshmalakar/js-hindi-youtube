6aba87fc1f26e7d3d0aeee6b772c288b77a8b472
Akhilesh Malakar <137974715+Akhileshmalakar@users.noreply.github.com>
2024-02-24 09:28:15 +0000

    arrays

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,18 @@
+// arrays 
+
+
+    //  () - perentheses  
+    //  [] - brackets 
+    //  {} - Curly braces 
+
+
+
+const array = [1,1,2,3,2,5,8]
+
+const myarr = ["ak","pappu"];
+
+const myarr2 = new array(1,2,3,4);
+
+console.log(array[2]);
+
+
