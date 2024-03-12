0a4cbf8294847e09f9c17426f56444beb6ddd00e
Akhilesh Malakar <137974715+Akhileshmalakar@users.noreply.github.com>
2024-03-12 04:23:50 +0000

    arrays basic

--- 02_basic/01_arrays.js
+++ 02_basic/01_arrays.js
@@ -9,10 +9,29 @@
 
 const array = [1,1,2,3,2,5,8]
 
-const myarr = ["ak","pappu"];
+// const myarr = ["ak","pappu"];
 
-const myarr2 = new array(1,2,3,4);
+// const myarr2 = new Array(1,2,34,3)
 
-console.log(array[2]);
+// console.log(array[2]);
 
+
+// arr Methods
+// array.push(6);
+// array.pop();
+
+// array.unshift(9);
+// array.shift();
+// console.log(array);
+
+// console.log(array.includes(17));
+// console.log(array.indexOf(3));
+
+
+const newArray = array.join();
+
+console.log(array);
+console.log(typeof array);
+console.log(newArray);
+console.log(typeof newArray);
 
