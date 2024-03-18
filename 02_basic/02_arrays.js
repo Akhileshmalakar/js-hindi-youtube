fc346d1155b20969dc629c3a17b6310468a1d646
Akhilesh Malakar <137974715+Akhileshmalakar@users.noreply.github.com>
2024-03-18 10:59:34 +0000

    arrays

---
+++ 02_basic/02_arrays.js
@@ -0,0 +1,39 @@
+// const bollywood_Heros = ["ajay", "hritik", "varun"];
+// const south_Heros = ["Alluarjun","Vijay","shingham"];
+
+// bollywood_Heros.push(south_Heros);
+
+// console.log(bollywood_Heros);
+
+
+const marvel_Heros = ["thor","Ironman","spiderman"];
+const dc_Heros = ["superman", "Flash", "batman"];
+
+// marvel_Heros.push(dc_Heros);
+// console.log(marvel_Heros);
+// console.log(marvel_Heros[3][1]);
+
+// const allHeros = marvel_Heros.concat(dc_Heros);
+// console.log(allHeros);
+
+// const all_New_Heros = [...marvel_Heros, ...dc_Heros];
+// console.log(all_New_Heros);
+
+// const another_array = [1,2,3,[4,5,6],7,[6,7,[7,9]]];
+// const real_another_array = another_array.flat(Infinity);
+// console.log(real_another_array);
+
+
+
+
+// console.log(Array.isArray("akku"));
+// console.log(Array.from("akku"));
+// console.log(Array.from({name: "akku"}));    // interesting
+
+
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1,score2,score3));
