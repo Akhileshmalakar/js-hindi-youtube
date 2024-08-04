2bfbf02d433cb007300c0bfdf5023a58f3f72429
Akhilesh Malakar <137974715+Akhileshmalakar@users.noreply.github.com>
2024-08-04 10:12:04 +0000

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,32 @@
+// // if 
+// const isUserloggedIn = true 
+
+// if( 2 == "2" ){
+//     console.log("true");
+// } 
+
+// const temperature = 52 
+// if ( temperature < 50 ){
+//     console.log("less than 50 ");
+// }
+// else {
+//     console.log("temp is greater than 50 ");
+// }
+
+// const score = 200
+// if (score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+const Balance = 1000 
+
+//if(Balance > 520) console.log("yes True ")
+
+
+const isUserloggedIn = true
+const debitCard = true
+
+if(isUserloggedIn && debitCard ){
+    console.log("Allo")
+}
