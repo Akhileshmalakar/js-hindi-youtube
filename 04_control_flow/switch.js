87ff7b3ca4ea217bf985ded06fa7e7aabfcd1a28
Akhilesh Malakar <137974715+Akhileshmalakar@users.noreply.github.com>
2024-08-04 12:29:35 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,35 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const day = 3
+
+switch (day) {
+    case 1:
+        console.log("sunday")
+        break;
+    case 2:
+        console.log("monday")
+        break;
+    case 3:
+        console.log("Tuesday")
+        break;
+    case 4:
+        console.log("wednesday")
+        break;
+    case 6:
+        console.log("thursday")
+        break;
+    case 7:
+        console.log("saturday")
+        break;
+
+    default:
+        console.log("default value ")
+        break;
+}
